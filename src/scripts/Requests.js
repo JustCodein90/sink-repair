abb61ec00b61fa7f2fa29e3628a54f9d79d4340c
Justin Ireland <justinireland90@yahoo.com>
2023-10-12 13:24:29 -0400

    commented out other values of request for later use. Added Delete button and eListener

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,11 +1,43 @@
 import { getRequests } from "./dataAccess.js";
+import { deleteRequest } from "./dataAccess.js";
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
 
 // Function converts request (parameter) array to HTML list items
 const convertRequestToList = (request) => {
-    let html = `<div class="listParent"> <li class="topList">Description: ${request.description}</li>
-                <li>Address: ${request.address}</li>
-                <li>Budget: ${request.budget}</li>
-                <li>Needed By: ${request.neededBy}</li>  </div>`
+    let html = `
+        <div class="listParent"> 
+            <li> 
+                Description: ${request.description} 
+                <button class="request__delete"
+                    id="request--${request.id}"> 
+                    Delete 
+                </button>
+            </li>
+        </div>` 
+            
+          // OTHER VALUES AVAILABLE IN THE REQUEST OBJECT///////
+          //ONLY NEED ONE BUTTON TO DELETE  WHOLE REQUEST////////
+
+            // <li> Address: ${request.address} </li>           
+            // <li> Budget: ${request.budget} </li>                                     
+                    
+            // <li>
+            //     Needed By: ${request.neededBy} 
+            //     <button class="request__delete"
+            //         id="request--${request.id}"> 
+            //         Delete 
+            //     </button>
+            // </li>         
+              
+                  
 
     return html
 }
